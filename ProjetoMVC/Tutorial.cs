053f3a64b3e7298b90e01c2bf72d3e1d8299e784
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-10-16 20:45:40 -0300

    ProjetoMVC
    UsuarioController

--- ProjetoMVC/Tutorial.cs
+++ ProjetoMVC/Tutorial.cs
@@ -13,12 +13,23 @@
         //***************************************************
         //Segundo passo é criar as classe na Models
         //geralmente espelhando as tabelas do banco de dados
+        //Dentro da pasta Models
         //OBS: Criar a classe de coleção junto
         //com a classe do objeto
         //Ex: Usuario e UsuarioCollection
         //***************************************************
         //Terceiro passo é criar a classe de conexão
         //com o banco de dados na camada de Serviço
+        //Dentro da pasta Services
+        //Ex: DataBaseService
         //***************************************************
+        //Quarto passo é criar a classe de controle dentro
+        //da pasta Controllers
+        //Ou seja iremos criar a classe que ira gerenciar
+        //o objeto com o banco de dados
+        //realizando o CRUD do objeto na conexão com o banco
+        //Ex: UsuarioController
+        //***************************************************
+
     }
 }
